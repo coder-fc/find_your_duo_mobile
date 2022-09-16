5becab992779bde47b22968ff2db2cd7949325a7
DevFC-M <felipech3344@gmail.com>
2022-09-16 00:23:20 -0300

    Criação do componente de Heading e inclusão dentro de home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 
 import {
-  View
+  View,
+  Image
 } from 'react-native';
 
-
+import logoImage from '../../assets/logo-nlw-esports.png'
+import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
 
 export function Home(){
   return (
-    <View>
+    <View style={styles.container}>
+      <Image 
+        source={logoImage}
+        style={styles.logo}
+      />
 
+    <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..."/>
+      
     </View>
   );
 }
