eab3d62ca7df956543b28492315c80179a43458e
DevFC-M <felipech3344@gmail.com>
2022-09-20 00:41:46 -0300

    Criação das rotas para navegação dentro do app com react navegation

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
   Inter_900Black
  } from '@expo-google-fonts/inter'
 
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
 
@@ -27,7 +27,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoader ? <Home /> : <Loading />}
+      { fontsLoader ? <Routes /> : <Loading />}
     </Background>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,20 +4,29 @@ import {
 } from 'react';
 
 import {
-  View,
+
   Image,
   FlatList
 } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNavigation } from '@react-navigation/native'
 
 import logoImage from '../../assets/logo-nlw-esports.png'
 
 import { GameCard, GameCardProps } from '../../components/GameCard';
+import { Background } from '../../components/Background';
 import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
 
 export function Home(){
   const [games, setGames] = useState<GameCardProps[]>([]);
+
+  const navigation = useNavigation()
+
+  function handleOpenGame({id, title, bannerUrl} : GameCardProps) {
+    navigation.navigate('game', { id, title, bannerUrl });
+  }
 
   useEffect(() => {
     fetch('http://192.168.43.130:19001/games')
@@ -27,28 +36,32 @@ export function Home(){
 
 
   return (
-    <View style={styles.container}>
-      <Image 
-        source={logoImage}
-        style={styles.logo}
-      />
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image 
+          source={logoImage}
+          style={styles.logo}
+        />
 
-      <Heading 
-        title="Encontre seu duo!" 
-        subtitle="Selecione o game que deseja jogar..."
-      />
+        <Heading 
+          title="Encontre seu duo!" 
+          subtitle="Selecione o game que deseja jogar..."
+        />
 
-      <FlatList 
-        data={games}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <GameCard data={item}
-          />
-        )}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.contentList}
-      />
-    </View>
+        <FlatList 
+          data={games}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <GameCard 
+              data={item}
+              onPress={ () => handleOpenGame(item)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.contentList}
+        />
+      </SafeAreaView>
+    </Background>
   );
 }
