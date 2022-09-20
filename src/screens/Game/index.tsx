4cc38ba012b75d13a48cc529872aa8eadb518a77
DevFC-M <felipech3344@gmail.com>
2022-09-20 02:20:57 -0300

    Finalizando a sessão de anuncios da screen de games com informações vindo da API

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react';
 import {
   TouchableOpacity, 
   View, 
-  Image
+  Image,
+  FlatList
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation, useRoute } from '@react-navigation/native';
@@ -10,12 +12,15 @@ import { Entypo } from '@expo/vector-icons'
 import logoImg from '../../assets/logo-nlw-esports.png'
 import { Background } from '../../components/Background';
 import { Heading } from '../../components/Heading';
+import { DuoCard, DuoCardProps} from '../../components/DuoCard';
 
 import { THEME } from '../../theme';
 import { GameParams } from '../../@types/@navigation';
 import { styles } from './styles';
 
 export function Game(){
+
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
   
   const navigation = useNavigation()
   const route = useRoute();
@@ -24,6 +29,12 @@ export function Game(){
   function handleGoBack() {
     navigation.goBack();
   }
+
+  useEffect(() => {
+    fetch(`http://192.168.43.130:19001/games/${game.id}/ads`)
+      .then(response => response.json())
+      .then(data => setDuos(data))
+  }, []);
 
   return (
     <Background>
@@ -57,6 +68,21 @@ export function Game(){
         <Heading 
           title={game.title}
           subtitle='Conecte-se e comece a jogar!'
+        />
+
+        <FlatList 
+          data={duos}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <DuoCard 
+              data={item} 
+              onConnect={() => { }}
+            />
+          )}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={styles.contentList}
+          showsHorizontalScrollIndicator={false}
         />
 
       </SafeAreaView>
