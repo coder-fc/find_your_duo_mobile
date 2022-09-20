eb7eeb57734d696fd3fb763f44874543519a83cd
DevFC-M <felipech3344@gmail.com>
2022-09-20 01:15:21 -0300

    Criação da sessão header da nova screen

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,14 +1,63 @@
 import {
-  SafeAreaView
+  TouchableOpacity, 
+  View, 
+  Image
 } from 'react-native';
-import { Background } from '../../components/Background';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { Entypo } from '@expo/vector-icons'
 
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { Background } from '../../components/Background';
+import { Heading } from '../../components/Heading';
+
+import { THEME } from '../../theme';
+import { GameParams } from '../../@types/@navigation';
 import { styles } from './styles';
 
 export function Game(){
+  
+  const navigation = useNavigation()
+  const route = useRoute();
+  const game = route.params as GameParams;
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <Background>
       <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity 
+            onPress={handleGoBack}
+          >
+            <Entypo 
+              name='chevron-thin-left'
+              size={20}
+              color={THEME.COLORS.CAPTION_300}
+            />
+
+          </TouchableOpacity>
+
+          <Image 
+            source={logoImg}
+            style={styles.logo}
+          />
+
+          <View style={styles.right}/>
+        </View>
+
+        <Image 
+          source={{uri: game.bannerUrl}}
+          style={styles.cover}
+          resizeMode="stretch"
+        />
+
+        <Heading 
+          title={game.title}
+          subtitle='Conecte-se e comece a jogar!'
+        />
 
       </SafeAreaView>
     </Background>
